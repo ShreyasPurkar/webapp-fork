60471c9e5d54fea3aaa026db3511d280c0a7e8e6
ishreyasp <purkarshreyas@gmail.com>
2025-02-18 11:52:17 -0500

    Update workflow script

--- src/test/java/com/webapp/shreyas_purkar_002325982/rest/resource/HealthCheckApiTest.java
+++ src/test/java/com/webapp/shreyas_purkar_002325982/rest/resource/HealthCheckApiTest.java
@@ -34,7 +34,7 @@ public class HealthCheckApiTest {
         .when()
                 .get("/healthz")
         .then()
-                .statusCode(201)
+                .statusCode(200)
                 .header("X-Content-Type-Options", "nosniff")
                 .header("Cache-Control", "no-cache, no-store, must-revalidate")
                 .header("Pragma", "no-cache");
