7cbf1cb9a456102e9453065becdfb97c93e54d1d
ishreyasp <purkarshreyas@gmail.com>
2025-04-10 13:25:24 -0400

    Add libraries to install in AMI

--- packer/updateOs.sh
+++ packer/updateOs.sh
@@ -16,6 +16,11 @@ sudo apt update && sudo apt upgrade -y
 log "Installing required packages..."
 sudo apt install -y openjdk-17-jdk 
 sudo apt install -y maven
-sudo apt install -y unzip
+sudo apt install -y unzip curl
+sudo apt install -y postgresql-client-16
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i amazon-cloudwatch-agent.deb
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+sudo apt install -y jq
